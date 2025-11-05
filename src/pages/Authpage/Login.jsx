f4b118a04c1839822b09bc8e67832e7e3b4e26de
Rohit <rk341190100@gmail.com>
2025-11-05 15:41:39 +0530

    removed

--- src/pages/Authpage/Login.jsx
+++ src/pages/Authpage/Login.jsx
@@ -27,7 +27,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch(`/auth/login`, {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
