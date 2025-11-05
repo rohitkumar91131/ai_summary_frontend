36f4b6957a9289bf284962a7ff99cfaa63c78439
Rohit <rk341190100@gmail.com>
2025-11-05 13:31:13 +0530

    added docker file

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -7,7 +7,6 @@ import { useAuth } from '../../context/AuthContext';
 function HomePage() {
     const {isLoggedIn , setIsLoggedIn , verifyingToken , setVerifyingToken} = useAuth();
     useEffect(() => {   
-        alert(process.env.REACT_APP_API_URL)
         async function checkAuth() {
             try {
                 const res = await fetch(`${process.env.REACT_APP_API_URL}/auth/verify`, {
